8c12162725f2143c77997aa80a15b344da71b6dc
Igor Daiko <igordaiko1604@gmail.com>
2024-02-04 16:58:07 +0200

    .

--- dotnet/RVocabular/build.sh
+++ dotnet/RVocabular/build.sh
@@ -4,12 +4,12 @@ dotnet publish -c Release -p:DebugType=None -p:DebugSymbols=false -p:PublishProf
 
 cd out && tar -czvf ../r-vocabular-host.tar.gz ./* && cd ..
 
-scp -i "private_key" ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_1}:/r-vocabular-host
-scp -i "private_key" ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_2}:/r-vocabular-host
+scp -o StrickHostKeyChecking=0 -i "private_key" ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_1}:/r-vocabular-host
+scp -o StrickHostKeyChecking=0 -i "private_key" ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_2}:/r-vocabular-host
 
 # echo $USER_NAME
 # echo $HOST_NAME_1
 # echo $HOST_NAME_2
 
-ssh -i "private_key" ${USER_NAME}@${HOST_NAME_1} "cd /r-vocabular-host; sudo ./run.sh"
-ssh -i "private_key" ${USER_NAME}@${HOST_NAME_2} "cd /r-vocabular-host; sudo ./run.sh"
+ssh -o StrickHostKeyChecking=0 -i "private_key" ${USER_NAME}@${HOST_NAME_1} "cd /r-vocabular-host; sudo ./run.sh"
+ssh -o StrickHostKeyChecking=0 -i "private_key" ${USER_NAME}@${HOST_NAME_2} "cd /r-vocabular-host; sudo ./run.sh"
