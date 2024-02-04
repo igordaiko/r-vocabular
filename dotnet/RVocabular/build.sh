4d37f0be049e4a9a40c8ef6d1f26cce52d7e6832
Igor Daiko <igordaiko1604@gmail.com>
2024-02-04 16:49:38 +0200

    .

--- dotnet/RVocabular/build.sh
+++ dotnet/RVocabular/build.sh
@@ -2,12 +2,12 @@ rm -rf out
 
 dotnet publish -c Release -p:DebugType=None -p:DebugSymbols=false -p:PublishProfile=default -p:TargetRuntimeIdentifier=linux-x64 ./RVocabular.Host -o ./out
 
-cd out && tar -czvf ../r-vocabular-host.tar.gz ./* && cd..
+cd out && tar -czvf ../r-vocabular-host.tar.gz ./* && cd ..
 
 cd ..\..\keys
 
-pscp -i private_key ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_1}:/r-vocabular-host
-pscp -i private_key ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_2}:/r-vocabular-host
+scp -i private_key ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_1}:/r-vocabular-host
+scp -i private_key ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_2}:/r-vocabular-host
 
 ssh -i private_key ${USER_NAME}@${HOST_NAME_1} "cd /r-vocabular-host; sudo ./run.sh"
 ssh -i private_key ${USER_NAME}@${HOST_NAME_2} "cd /r-vocabular-host; sudo ./run.sh"
