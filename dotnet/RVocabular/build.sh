cdad39f4d91cf5ca4f9e4eadb207c21a423df508
Igor Daiko <igordaiko1604@gmail.com>
2024-02-04 17:04:14 +0200

    .

--- dotnet/RVocabular/build.sh
+++ dotnet/RVocabular/build.sh
@@ -2,10 +2,10 @@ rm -rf out
 
 dotnet publish -c Release -p:DebugType=None -p:DebugSymbols=false -p:PublishProfile=default -p:TargetRuntimeIdentifier=linux-x64 ./RVocabular.Host -o ./out
 
-cd out && tar -czvf ../r-vocabular-host.tar.gz ./* && cd ..
+cd out && tar -czvf ./r-vocabular-host.tar.gz ./* && cd ..
 
-scp -o StrictHostKeyChecking=no -i "private_key" ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_1}:/r-vocabular-host
-scp -o StrictHostKeyChecking=no -i "private_key" ../dotnet/RVocabular/r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_2}:/r-vocabular-host
+scp -o StrictHostKeyChecking=no -i "private_key" ./r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_1}:/r-vocabular-host
+scp -o StrictHostKeyChecking=no -i "private_key" ./r-vocabular-host.tar.gz ${USER_NAME}@${HOST_NAME_2}:/r-vocabular-host
 
 # echo $USER_NAME
 # echo $HOST_NAME_1
